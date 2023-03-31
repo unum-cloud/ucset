88a679279639959102d318481911e28530dc2786
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-03-31 19:47:07 +0400

    Docs: Change GA tracking number to GA4

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@ with open('_static/custom.js', 'r+') as js:
     js.seek(0)
     js.truncate()
     js.write(content.replace("$(VERSION)", release))
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
@@ -30,7 +31,7 @@ extensions = [
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '*.md']
 
 
-googleanalytics_id = 'UA-150644745-1'
+googleanalytics_id = '341385789'
 googleanalytics_enabled = True
 
 # -- Options for HTML output -------------------------------------------------
@@ -39,12 +40,8 @@ googleanalytics_enabled = True
 html_logo = '../assets/unum.png'
 html_theme = 'furo'
 html_static_path = ['_static']
-html_css_files = [
-    'custom.css'
-]
-html_js_files = [
-    'custom.js'
-]
+html_css_files = ['custom.css']
+html_js_files = ['custom.js']
 
 
 breathe_projects = {'UCSet': '../build/xml'}
